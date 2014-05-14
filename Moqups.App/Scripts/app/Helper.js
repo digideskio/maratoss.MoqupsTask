5df0eff293d53a6b43edb69eecf97f805b15d431
maratoss <imaratoss@gmail.com>
2014-05-14 17:27:24 +0400

    revert commit

---
+++ Moqups.App/Scripts/app/Helper.js
@@ -0,0 +1,48 @@
+﻿function fillData(model) {
+    for (var i = 0; i < 10; i++) {
+        var user = new User(i, 'fName: ' + i, 'lName: ' + i);
+        user.Pages.push(new Page(1, 'Page 1'));
+        user.Pages.push(new Page(2, 'Page 2'));
+        model.Users.push(user);
+
+        if (i % 2 == 0) {
+            user.Pages.push(new Page(3, 'Page 3'));
+        }
+    }
+}
+
+var NavigateManager = function () {
+    var self = this;
+    self.OpenInNewWindow = function (viewModel) {
+        //todo: download view and bind viewmodel
+        var view = $('#dialog');
+        $.ajax({
+            type: "POST",
+            url: 'AddOrEditForm.html',
+            error: function (data) {
+                view.html(data.responseText);
+                //alert(data.responseText);
+            },
+            success: function (data) {
+                view.html(data);
+                view.dialog({
+                    modal: true,
+                    title: 'some title',
+                    width: 350
+                });
+                view.html(data);
+                //BindData(viewModel);
+                view.dialog();
+            }
+        });
+    };
+    self.RegisterView = function (view, viewModel, contract) {
+        //todo: register view and viewmodel
+    };
+};
+
+function getType(val) {
+    if (typeof val === 'undefined') return 'undefined';
+    if (typeof val === 'object' && !val) return 'null';
+    return ({}).toString.call(val).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
+}
---
+++ Moqups.App/Scripts/app/Model.js
@@ -0,0 +1,45 @@
+﻿var User = function (id, fName, lName) {
+    var self = this;
+    self.Id = id;
+    self.Firstname = ko.observable(fName);
+    self.Lastname = ko.observable(lName);
+    self.Status = ko.observable(1);
+    self.Pages = ko.observableArray();
+};
+
+var Page = function (id, name) {
+    var self = this;
+    self.Id = id;
+    self.Name = name;
+};
+
+var UserViewModel = function (navigateManager) {
+    var self = this;
+    self.Users = ko.observableArray();
+    self.GoToAddUserCommand = function () {
+        var user = new User();
+        var editUserViewModel = new EditUserViewModel(user);
+        navigateManager.OpenInNewWindow(editUserViewModel);
+    };
+    self.GoToEditUserCommand = function (newUser) {
+        alert('GoToEditUserCommand');
+    };
+    self.DeleteUserCommand = function (id) {
+        alert('DeleteUserCommand');
+    };
+};
+
+var EditUserViewModel = function (user) {
+    var self = this;
+    self.User = ko.observable(user);
+    self.AvaiablePages = ko.observableArray();
+    self.AddUserCommand = function () {
+        alert('AddUserCommand');
+    };
+    self.EditUserCommand = function () {
+        alert('EditUserCommand');
+    };
+    self.RemoveUserCommand = function () {
+        alert('RemoveUserCommand');
+    };
+}
